096680b01f07119ea304e7f97e02cfa8696b7385
KwanwooKo <kwko97@gmail.com>
2022-07-31 17:04:05 +0900

    최단 경로 part2 1번

---
+++ part2/최단 경로/1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <stack>
+#include <cstring>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    int start;
+    cin >> start;
+    vector<int> edges;
+    for (int i = 0; i < m; i++) {
+        int from, to;
+        cin >> from >> to;
+    }
+    return 0;
+}
